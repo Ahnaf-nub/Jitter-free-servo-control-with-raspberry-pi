fda5a9474dcd51ae33d75559a3ad276e211cba88
Mir Muhammad Abidul Haq (Ahnaf) <76505613+Ahnaf337@users.noreply.github.com>
2022-07-09 10:49:14 +0600

    Update Jitter-free-servo-control-with-raspberry-pi.py

--- Jitter-free-servo-control-with-raspberry-pi.py
+++ Jitter-free-servo-control-with-raspberry-pi.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import pigpio
 import time 
-servo = 13
-pwm = pigpio.pi()
+servo = 13 #কোন পিন এ কানেক্ট করেছি সেটা বলে দিলাম। 
+pwm = pigpio.pi() 
 pwm.set_mode(servo, pigpio.OUTPUT)
-pwm.set_PWM_frequency(servo, 50)
+pwm.set_PWM_frequency(servo, 50) #PWM ফ্রিকুএন্সি সেট করে দিলাম এটা ৫০ রাখলেই ভাল। 
 print("0 deg")
 pwm.set_servo_pulsewidth(servo, 500) ;
 time.sleep(3)
